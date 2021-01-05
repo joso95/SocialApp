05dac19fddb9ab0e74f79049d084a7b274d6df69
Jorgen Solum <j-solum@live.com>
2021-01-05 19:51:32 +0100

    creation and connection
    creation of the project has been done, connection to mongodb has been established.

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const  { mode, schema} = require('mongoose');
+
+const userSchema = new Schema({
+    username: String,
+    password: String,
+    email: String,
+    createdAt: String
+});
+
+module.exports = model('User', userSchema);
